8703804d1606dfa4803d99d56e7a9b1a45b22457
ningque <3399693673@qq.com>
2019-04-15 09:42:00 +0800

    ddd

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,3 +2,23 @@ const image = require('gulp-image');
 const gulp = require('gulp');
 const css = require('gulp-clean-css');
 const js = require('gulp-uglify');
+
+
+//压缩css
+gulp.task('css',()=>{
+    gulp.src('css/*.css')
+    .pipe(css())
+    .pipe(dest(dist))
+})
+//压缩js
+gulp.task('js',()=>{
+    gulp.src('js/*.js')
+    .pipe(js())
+    .pipe(dest(dist))
+})
+//压缩image
+gulp.task('image',()=>{
+    gulp.src('images/*.png')
+    .pipe(image())
+    .pipe(dest(dist))
+})
